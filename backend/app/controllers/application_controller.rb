f59b9c662e92121b1aef48709c915b5c50a124b2
yuito-syz <yuito_sy1122@icloud.com>
2022-05-21 13:27:50 +0900

    Rails7

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,14 +1,2 @@
 class ApplicationController < ActionController::API
-  include SessionsHelper
-  
-  private
-
-    # ユーザーのログインを確認する
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
 end
