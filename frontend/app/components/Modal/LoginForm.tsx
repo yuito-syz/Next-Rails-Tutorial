2455877a40813c98800778625f00545a85a65cf3
yuito-syz <yuito_sy1122@icloud.com>
2022-05-24 21:01:12 +0900

    setting frontend

---
+++ frontend/app/components/Modal/LoginForm.tsx
@@ -0,0 +1,101 @@
+import { useForm } from 'react-hook-form';
+import { useRouter } from 'next/router';
+import { mutate } from 'swr'
+//module
+import { Auth } from '../../modules/Auth'
+//hooks
+import { AutoLoginUrl } from '../../hooks/useUserSWR'
+import { useFlashReducer } from "../../hooks/useFlashReducer";
+//types
+import { LoginValueType, UserLoginType } from '../../types/UserType'
+//othres
+import Button from 'react-bootstrap/Button'
+import styles from './Form.module.scss';
+
+const endpoint = process.env.NEXT_PUBLIC_BASE_URL + 'login'
+
+type LoginFormProps = {
+  Closemodal: VoidFunction,
+  PasswordResetControll: VoidFunction
+}
+
+export const LoginForm: React.FC<LoginFormProps> = ({ Closemodal, PasswordResetControll }) => {
+
+  //useFlashReducerを読み込み
+  const { FlashReducer } = useFlashReducer()
+
+  const router = useRouter()
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  // appからcontextsを受け取る
+
+  const onSubmit = (value: LoginValueType) => {
+    fetch(endpoint, {
+      method: 'POST', // or 'PUT'
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: value.email,
+        password: value.password,
+      }),
+    })
+      .then(response => response.json())
+      .then((data): UserLoginType | undefined => {
+        // console.log('response data')
+        // console.log(data);
+        if (data.error) {
+          console.log(data.error);
+          FlashReducer({ type: "DANGER", message: data.error })
+          Closemodal()
+          return
+        }
+        //Login関連の処理 context使用
+        Auth.login(data.token);
+        //user data更新
+        mutate(AutoLoginUrl)
+        //Login関連の処理 終了
+        const user_data = data.user
+        // router.push(`/users/${user_data.id}`);
+        FlashReducer({ type: "SUCCESS", message: `Welcome back ${user_data.name}` })
+        Closemodal()
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+  return (<>
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+      <label className={styles.label} htmlFor="email">Email</label>
+      <input
+        id="email"
+        className={styles.form_input}
+        // name="email"
+        type="email"
+        aria-invalid={errors.email ? "true" : "false"}
+        {...register('email', { required: true })}
+      />
+      {errors.email && (
+        <span role="alert" className={styles.alert}>
+          This field is required
+        </span>
+      )}
+      <label className={styles.label} htmlFor="password">Password</label>
+      <input
+        id="password"
+        className={styles.form_input}
+        type="password"
+        // name="password"
+        aria-invalid={errors.password ? "true" : "false"}
+        {...register('password', { required: true })}
+      />
+
+      {errors.password && (
+        <span role="alert" className={styles.alert}>
+          This field is required
+        </span>
+      )}
+      <Button variant="outline-warning" size='sm' onClick={() => PasswordResetControll()}>Forget the Password?</Button>
+      <button type="submit" className={styles.form_submit}>Log in</button>
+    </form>
+  </>)
+}
---
+++ frontend/app/components/Modal/UserForm.tsx
@@ -0,0 +1,122 @@
+import { useForm } from 'react-hook-form';
+import { mutate } from 'swr'
+//types
+import { UserValueType, UserSignupType } from '../../types/UserType'
+//hooks
+import { useFormErrors } from '../../hooks/useFormErrors'
+import { AutoLoginUrl } from '../../hooks/useUserSWR'
+import { useFlashReducer } from '../../hooks/useFlashReducer';
+//module
+import { Auth } from '../../modules/Auth'
+//othres
+import styles from './Form.module.scss';
+// import { FlashMessageContext } from '../../pages/_app'
+
+const endpoint = process.env.NEXT_PUBLIC_BASE_URL + 'users'
+
+type UserFormProps = {
+  Closemodal: VoidFunction
+}
+
+export const UserForm: React.FC<UserFormProps> = ({ Closemodal }) => {
+  const { register, handleSubmit } = useForm();
+
+  //useFlashReducerを読み込み
+  const { FlashReducer } = useFlashReducer()
+
+
+  const initialerrors = { name: '', email: '', password: '', password_confirmation: '' };
+  const { errors, handleError, resetError } = useFormErrors(initialerrors)
+
+  const onSubmit = (value: UserValueType): void => {
+    fetch(endpoint, {
+      method: 'POST', // or 'PUT'
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        user: {
+          name: value.name,
+          email: value.email,
+          password: value.password,
+          password_confirmation: value.password_confirmation
+        }
+      }),
+    })
+      .then(response => response.json())
+      .then((data): UserSignupType | undefined => {
+        // console.log('response data')
+        // console.log({ data });
+        if (data.errors) {
+          FlashReducer({ type: "DANGER", message: "Your form is invalid!" })
+          handleError(data.errors);
+          return
+        }
+        // console.log(data.token);
+        //Login関連の処理 context使用
+        Auth.login(data.token);
+        //SWRのユーザーデータを更新しておく
+        mutate(AutoLoginUrl)
+        const user_data = data.user
+        FlashReducer({ type: "PRIMARY", message: `Welcome to Sample App, ${user_data.name}` })
+        // router.push('/reviews/new')
+        Closemodal()
+        //Login関連の処理 終了
+        resetError();
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+  return (
+    <>
+      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+        <label className={styles.label} htmlFor="name">Your name</label>
+        <input
+          className={styles.form_input}
+          id="name"
+          // name="name"
+          {...register('name', { required: true })}
+        />
+        <label className={styles.label} htmlFor="email">Email</label>
+        <input
+          id="email"
+          className={styles.form_input}
+          // name="email"
+          type="email"
+          {...register('email', { required: true })}
+        />
+        {errors.email !== '' && (
+          <p className={styles.form_error}>Email {errors.email}</p>
+        )}
+        <label className={styles.label} htmlFor="password">Password</label>
+        <input
+          id="password"
+          className={styles.form_input}
+          type="password"
+          // name="password"
+          {...register('password', { required: true })}
+        />
+        {errors.password !== '' && (
+          <p className={styles.form_error}>Password {errors.password}</p>
+        )}
+        <label
+          className={styles.label}
+          htmlFor="password_confirmation">
+          Password_confirmation
+        </label>
+        <input
+          id="password_confirmation"
+          className={styles.form_input}
+          type="password"
+          // name="password_confirmation"
+          {...register('password_confirmation', { required: true })}
+        />
+        {errors.password_confirmation !== "" && (
+          <p className={styles.form_error}>Password_confirmation {errors.password_confirmation}</p>
+        )}
+        <button type="submit" className={styles.form_submit}>Sign up</button>
+      </form>
+    </>
+  )
+}
