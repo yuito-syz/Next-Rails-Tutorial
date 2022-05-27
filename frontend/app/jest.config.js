4c391356979c61e285827d91a73b1b2a081e2d18
yuito-syz <yuito_sy1122@icloud.com>
2022-05-27 19:27:57 +0900

    frontend

---
+++ frontend/app/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+    roots: ["<rootDir>"],
+    testEnvironment: "jsdom",
+    moduleFileExtensions: ["ts", "tsx", "js", "json", "jsx"],
+    testPathIgnorePatterns: ["<rootDir>[/\\\\](node_modules|.next)[/\\\\]"],
+    transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$"],
+    transform: {
+      "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
+    },
+    moduleNameMapper: {
+      "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+    },
+};
